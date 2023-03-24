0a3a3adb82d4ba68006771b449c92596b0a4979c
yaroslav-tsarenko <yaroslav7v@gmail.com>
2023-03-24 20:54:49 +0200

    refactored MySpringMvcDispatcherServletIntitializer

--- src/main/java/yaroslav/springcourse/config/MySpringMvcDispatcherServletIntitializer.java
+++ src/main/java/yaroslav/springcourse/config/MySpringMvcDispatcherServletIntitializer.java
@@ -2,19 +2,19 @@ package yaroslav.springcourse.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class MySpringMvcDispatcherServletIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return null;
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {SpringConfig.class};
+        return new Class[]{SpringConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[] {"/"};
+        return new String[]{"/"};
     }
 }
